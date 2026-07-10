c13a3c2ae896144cc17bbd98ae009f93a105d28b
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-07-10 16:37:47 -0300

    feat: smart components testing

--- bootcamp_react_ia/my-first-component/src/components/ProductsLit/index.jsx
+++ bootcamp_react_ia/my-first-component/src/components/ProductsLit/index.jsx
@@ -3,21 +3,46 @@ export default function ProductsList() {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-
+    setIsLoading(true);
+    fetch("`${API_BASE_URL}/api/products`")
+      .then(async (response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setProducts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        alert("Erro ao buscar produtos: " + error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
-    
+
+  if (isLoading) {
+    return;
+    <div>
+      <h2>Produtos</h2>
+      <p>Carregando...</p>
+    </div>;
+  }
 
   return (
     <div>
       <h2>Produtos</h2>
       <ul className="list">
-        <li>
-          <p>#123 Nome do Produto</p>
-          <p className="price">R$ 100,00</p>
-          <p className="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-          </p>
-        </li>
+        {products.map((id, name, price, description) => (
+          <li key={id}>
+            <p>
+              #{id} {name}
+            </p>
+            <p className="price">R$ {price.toFixed(2)}</p>
+            <p>{description}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
