99b8d901acce2db9cfceb211f2fb065013da665a
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-08-18 09:51:23 -0300

    feat: create planejai project

--- bootcamp_react_ia/cra/my-app-ts/src/settings.ts
+++
@@ -1,3 +0,0 @@
-export const ENABLE_LOG = process.env.REACT_APP_ENABLE_LOG === '1' || false;
-export const ORGANIZATION_NAME = process.env.REACT_APP_ORGANIZATION_NAME || 'Unknown Organization';
-export const RELEASE_DATE = process.env.REACT_APP_RELEASE_DATE || 'Unknown Release Date';
---
+++ bootcamp_react_ia/planejai/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>Planejai</h1>
+    </>
+  )
+}
+
+export default App
